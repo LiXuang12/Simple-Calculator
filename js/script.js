cb3594628591c21fa6a1a795128dced0b5265ed4
LiXuang12 <muhfarisassami@gmail.com>
2024-02-13 02:59:43 +0700

    a

--- js/script.js
+++ js/script.js
@@ -4,10 +4,31 @@ function addToScreen(input) {
     display.value += input;
 }
 
-function clear() {
+function clearDisplay() {
     display.value = "";
 }
 
 function calculate() {
     display.value =  eval(display.value);
 }
+
+function deleteNum() {
+    let currentDisplay = display.value;
+    let updateDisplay = currentDisplay.slice(0, -1);
+    display.value = updateDisplay;
+}
+
+document.addEventListener("keydown", function(event) {
+    if ((event.key >= "0" && event.key <= "9") || event.key === "." || event.key === "+" || event.key === "-" || event.key === "*" || event.key === "/") {
+        addToScreen(event.key);
+    } else if (event.key === "c") {
+        clearDisplay();
+    }else if(event.key === "Enter") {
+        calculate();
+    }else if(event.key === "Backspace"){
+        deleteNum();
+    }
+});
+
+
+
